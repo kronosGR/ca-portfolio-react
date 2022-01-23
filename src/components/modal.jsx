16879b9f12f4e6058d3349c8722ca55a2238db5b
kronosGR <geo.elgeo@gmail.com>
2022-01-23 10:16:55 +0100

    added click on modal to close

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,10 +2,16 @@ import Zoom from 'react-reveal/Zoom';
 import classes from './modal.module.css';
 
 function Modal(props) {
+  const handleClick = (e) => {
+    if (e.target.className.includes('modal_modal')){
+      props.onClose();
+    }
+  };
+
   if (!props.isOpen) return null;
   return (
     <Zoom>
-      <div className={classes.modal}>
+      <div className={classes.modal} onClick={handleClick}>
         <div onClick={props.onClose} className={classes.modal_btn_close}>
           Close
         </div>
